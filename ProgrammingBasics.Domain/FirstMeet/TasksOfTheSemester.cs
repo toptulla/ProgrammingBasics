9945b0e4609a88d88cc3f9ecb0c7043cbfc74e3b
ad.tsay <ad.tsay@outlook.com>
2020-03-07 17:22:33 +0500

    T1. Swap двух натуральных чисел.

---
+++ ProgrammingBasics.Domain/FirstMeet/TasksOfTheSemester.cs
@@ -0,0 +1,26 @@
+﻿namespace ProgrammingBasics.Domain.FirstMeet
+{
+    public class TasksOfTheSemester
+    {
+        /// <summary>
+        /// Как поменять местами значения двух переменных?
+        /// Можно ли это сделать без ещё одной временной переменной. Стоит ли так делать?
+        /// </summary>
+        public (uint X, uint Y) SwapNaturalNumbersWithoutTmp(uint x, uint y)
+        {
+            /*
+             * Это возможно.
+             * Так делать не стоит - код усложняется, его становится труднее "читать", а затем и поддерживать.
+            */
+
+            checked
+            {
+                x += y;
+                y = x - y;
+                x = x - y;
+            }
+
+            return (x, y);
+        }
+    }
+}
---
+++ ProgrammingBasics.Tests/FirstMeet/TasksOfTheSemesterTests.cs
@@ -0,0 +1,28 @@
+﻿using ProgrammingBasics.Domain.FirstMeet;
+using System;
+using Xunit;
+
+namespace ProgrammingBasics.Tests.FirstMeet
+{
+    public class TasksOfTheSemesterTests
+    {
+        [Fact]
+        public void SwapNaturalNumbersWithoutTmpSimpleTest()
+        {
+            var task = new TasksOfTheSemester();
+
+            var result = task.SwapNaturalNumbersWithoutTmp(x: 10, y: 20);
+
+            Assert.Equal<uint>(20, result.X);
+            Assert.Equal<uint>(10, result.Y);
+        }
+
+        [Fact]
+        public void SwapNaturalNumbersWithoutTmpMaxIntTest()
+        {
+            var task = new TasksOfTheSemester();
+
+            Assert.Throws<OverflowException>(() => task.SwapNaturalNumbersWithoutTmp(x: uint.MaxValue, y: 20));
+        }
+    }
+}
