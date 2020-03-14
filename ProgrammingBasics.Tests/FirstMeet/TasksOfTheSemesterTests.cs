520471ea774078a08b5b69fb4fcffac33a38ae69
toptulla <ad.tsay@outlook.com>
2020-03-14 12:04:42 +0500

    Seminar 1 tasks (#1)
    * T1. Swap двух натуральных чисел.
    
    * T6. Расчет расстояния от точки до прямой на плоскости.
    
    * T5. Расчет количества високосных лет на отрезке.
    
    * T4. Найти количество чисел меньших N, которые имеют простые делители X или Y.
    
    * T3. Вычислить угол в градусах между часовой и минутной стрелками.
    
    * T2. Развернуть трехзначное число.
    
    * fix для T4
    
    * T8. Найти точку пересечения прямой L с перпендикулярной ей прямой P, проходящей через точку A.

---
+++ ProgrammingBasics.Tests/FirstMeet/TasksOfTheSemesterTests.cs
@@ -0,0 +1,134 @@
+﻿using ProgrammingBasics.Domain.FirstMeet;
+using System;
+using Xunit;
+
+namespace ProgrammingBasics.Tests.FirstMeet
+{
+    public class TasksOfTheSemesterTests
+    {
+        #region Task 1
+        [Fact]
+        public void SwapNaturalNumbersWithoutTmpSimpleTest()
+        {
+            var task = new TasksOfTheSemester();
+
+            var result = task.SwapNaturalNumbersWithoutTmp(x: 10, y: 20);
+
+            Assert.Equal<uint>(20, result.X);
+            Assert.Equal<uint>(10, result.Y);
+        }
+
+        [Fact]
+        public void SwapNaturalNumbersWithoutTmpMaxIntTest()
+        {
+            var task = new TasksOfTheSemester();
+
+            Assert.Throws<OverflowException>(() => task.SwapNaturalNumbersWithoutTmp(x: uint.MaxValue, y: 20));
+        }
+        #endregion
+
+        #region Task 2
+        [Fact]
+        public void RevertNumberSimpleTest()
+        {
+            var task = new TasksOfTheSemester();
+
+            var result = task.RevertNumber(123);
+
+            Assert.Equal<int>(321, result);
+        }
+        #endregion
+
+        #region Task 3
+        [Fact]
+        public void GetAngelBetweenHourAndMinuteHandSimpleTest15()
+        {
+            var task = new TasksOfTheSemester();
+
+            var result = task.GetAngelBetweenHourAndMinuteHand(15);
+
+            Assert.Equal<int>(90, result);
+        }
+
+        [Fact]
+        public void GetAngelBetweenHourAndMinuteHandSimpleTest9()
+        {
+            var task = new TasksOfTheSemester();
+
+            var result = task.GetAngelBetweenHourAndMinuteHand(9);
+
+            Assert.Equal<int>(90, result);
+        }
+
+        [Fact]
+        public void GetAngelBetweenHourAndMinuteHandSimpleTest0()
+        {
+            var task = new TasksOfTheSemester();
+
+            var result = task.GetAngelBetweenHourAndMinuteHand(20);
+
+            Assert.Equal<int>(120, result);
+        }
+        #endregion
+
+        #region Task 4
+        [Fact]
+        public void GetCountNumbersLessNSimpleTest()
+        {
+            var task = new TasksOfTheSemester();
+
+            var result = task.GetCountNumbersLessN(16, 3, 5);
+
+            Assert.Equal<int>(7, result);
+        }
+        #endregion
+
+        #region Task 5
+        [Fact]
+        public void CountLeapYearsSimpleTest()
+        {
+            var task = new TasksOfTheSemester();
+
+            var result = task.CountLeapYears(2019, 2021);
+
+            Assert.Equal<int>(1, result);
+        }
+        #endregion
+
+        #region Task 6
+        [Fact]
+        public void CalcPathFormPointToLineSimpleTest()
+        {
+            var task = new TasksOfTheSemester();
+
+            var result = task.CalcPathFormPointToLine((2, 2), (1, 1), (3, 1));
+
+            Assert.Equal<double>(1, result);
+        }
+        #endregion
+
+        #region Task 8
+        [Fact]
+        public void GetIntersectionPointSimpleTest0()
+        {
+            var task = new TasksOfTheSemester();
+
+            var result = task.GetIntersectionPoint((2, 2), (0, 2), (2, 0));
+
+            Assert.Equal<double>(1, result.x);
+            Assert.Equal<double>(1, result.y);
+        }
+
+        [Fact]
+        public void GetIntersectionPointSimpleTest1()
+        {
+            var task = new TasksOfTheSemester();
+
+            var result = task.GetIntersectionPoint((2, 2), (1, 1), (3, 1));
+
+            Assert.Equal<double>(2, result.x);
+            Assert.Equal<double>(1, result.y);
+        }
+        #endregion
+    }
+}
