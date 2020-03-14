e132e4930046949d5641d506cfd3aaf55151b51c
ad.tsay <ad.tsay@outlook.com>
2020-03-14 11:36:13 +0500

    Задача с лекций. Нахождение минимума квадратичной функции

---
+++ ProgrammingBasics.Domain/Errors/LectureTasks.cs
@@ -0,0 +1,26 @@
+﻿namespace ProgrammingBasics.Domain.Errors
+{
+    public class LectureTasks
+    {
+        /// <summary>
+        /// Определить минимум функции ax^2 + bx + c, а если его не существует,
+        /// то вернуть "Impossible".
+        /// </summary>
+        /// <param name="a">a.</param>
+        /// <param name="b">b.</param>
+        /// <param name="c">c.</param>
+        /// <returns>Строковое представление min функции или "Impossible"/"Any"</returns>
+        public string GetMinX(int a, int b, int c)
+        {
+            if (a == 0 && b == 0)
+                return "Any";
+
+            if (a <= 0)
+                return "Impossible";
+
+            double x = -1 * (double)b / (2 * a);
+
+            return x.ToString();
+        }
+    }
+}
