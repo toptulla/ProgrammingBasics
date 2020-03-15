5d9b0e6bce4841e2f83fff2804e62e71359ae099
ad.tsay <ad.tsay@outlook.com>
2020-03-15 12:57:14 +0500

    T1. Найти сумму всех положительных чисел меньше 1000 кратных 3 или 5.

---
+++ ProgrammingBasics.Tests/Errors/TasksForSeminarTests.cs
@@ -0,0 +1,20 @@
+﻿using ProgrammingBasics.Domain.Errors;
+using Xunit;
+
+namespace ProgrammingBasics.Tests.Errors
+{
+    public class TasksForSeminarTests
+    {
+        #region Task 1
+        [Fact]
+        public void SumNumbersMultiplesFiveAndThreeSimpleTest()
+        {
+            var task = new TasksForSeminar();
+
+            var result = task.SumNumbersMultiplesFiveAndThree(15);
+
+            Assert.Equal(60, result);
+        }
+        #endregion
+    }
+}
