2712fa473eafcac9a5b0920eb021626519bdb78b
ad.tsay <ad.tsay@outlook.com>
2020-03-21 09:29:22 +0500

    T3, T4. Задача 'Комфорт пассажиров' и 'Пусти козла в огород'.

--- ProgrammingBasics.Tests/Errors/TasksForSeminarTests.cs
+++ ProgrammingBasics.Tests/Errors/TasksForSeminarTests.cs
@@ -38,5 +38,34 @@ namespace ProgrammingBasics.Tests.Errors
             Assert.Equal<double>(150, result);
         }
         #endregion
+
+        #region Task 3
+        [Theory]
+        [InlineData(10000, 500, 50, 10, 125, 500)]
+        public void MinMaxTSimpleTest(int h, int t, int v, int x, double tmin, double tmax)
+        {
+            var task = new TasksForSeminar();
+
+            var result = task.MinMaxT(h, t, v, x);
+
+            Assert.Equal(tmin, result.minT);
+            Assert.Equal(tmax, result.maxT);
+        }
+        #endregion
+
+        #region Task 4
+        [Theory]
+        [InlineData(10, 6, 95.091)]
+        [InlineData(10, 5, 78.539)]
+        [InlineData(10, 10, 100)]
+        public void GetCircleSquareIntersectionAreaSimpleTest(double a, double r, double expectedArea)
+        {
+            var task = new TasksForSeminar();
+
+            var actualArea = task.GetCircleSquareIntersectionArea(a, r);
+
+            Assert.Equal<double>(expectedArea, actualArea);
+        }
+        #endregion
     }
 }
