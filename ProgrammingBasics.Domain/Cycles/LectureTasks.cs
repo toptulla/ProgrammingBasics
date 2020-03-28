dbc646ca7f9fe4161945f510227610bbf8d7c635
ad.tsay <ad.tsay@outlook.com>
2020-03-28 17:14:33 +0500

    Задачи с лекций о циклах. Начальная работа со строками.

---
+++ ProgrammingBasics.Domain/Cycles/LectureTasks.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingBasics.Domain.Cycles
+{
+    public class LectureTasks
+    {
+        public string RemoveSpaces(string text)
+        {
+            return text.TrimStart();
+        }
+
+        public (string fl, string sl, string tl) GetFramedText(string text)
+        {
+            string l = new string('-', text.Length);
+
+            string fl = string.Format("+-{0}-+", l);
+            string sl = string.Format("| {0} |", text);
+            string tl = string.Format("+-{0}-+", l);
+
+            return (fl, sl, tl);
+        }
+
+        public string[] GenerateChessBoard(int size)
+        {
+            var chessBoardLines = new string[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                var line = new char[size];
+                
+                for (int j = 0; j < size; j++)
+                    line[j] = (i + j) % 2 == 0 ? '#' : '.';
+
+                chessBoardLines[i] = new string(line);
+            }
+
+            return chessBoardLines;
+        }
+    }
+}
---
+++ ProgrammingBasics.Tests/Cycles/LectureTaskTests.cs
@@ -0,0 +1,56 @@
+﻿using Xunit;
+using ProgrammingBasics.Domain.Cycles;
+
+namespace ProgrammingBasics.Tests.Cycles
+{
+    public class LectureTaskTests
+    {
+        [Theory]
+        [InlineData("a", "a")]
+        [InlineData(" b", "b")]
+        [InlineData(" cd", "cd")]
+        [InlineData(" efg", "efg")]
+        [InlineData(" text", "text")]
+        [InlineData(" two words", "two words")]
+        [InlineData("  two spaces", "two spaces")]
+        [InlineData("	tabs", "tabs")]
+        [InlineData("		two	tabs", "two	tabs")]
+        [InlineData("                             many spaces", "many spaces")]
+        [InlineData("               ", "")]
+        [InlineData("\n\r line breaks are spaces too", "line breaks are spaces too")]
+        public void RemoveSpacesSimpleTests(string text, string expected)
+        {
+            var tasks = new LectureTasks();
+
+            var actual = tasks.RemoveSpaces(text);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("Hello world", "+-------------+", "| Hello world |", "+-------------+")]
+        public void GetFramedTextSimpleTest(string text, string expectedFl, string expectedSl, string expectedTl)
+        {
+            var tasks = new LectureTasks();
+
+            var actual = tasks.GetFramedText(text);
+
+            Assert.Equal(expectedFl, actual.fl);
+            Assert.Equal(expectedSl, actual.sl);
+            Assert.Equal(expectedTl, actual.tl);
+        }
+
+        [Theory]
+        [InlineData(1, new[] { "#" })]
+        [InlineData(2, new[] { "#.", ".#" })]
+        [InlineData(3, new[] { "#.#", ".#.", "#.#" })]
+        public void GenerateChessBoardSimpleTest(int sideSize, string[] expectedChessBoardLines)
+        {
+            var tasks = new LectureTasks();
+
+            var actualChessBoardLines = tasks.GenerateChessBoard(sideSize);
+
+            Assert.Equal(expectedChessBoardLines, actualChessBoardLines);
+        }
+    }
+}
