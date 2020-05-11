3f8f4c7787113cb793bbf46ebfbc86c99df267c0
toptulla <ad.tsay@outlook.com>
2020-05-11 17:35:14 +0500

    Arrays (#4)
    * Задачи с лекций по массивам. Первые две задачи с семинара по массивам.
    
    * Задача крестики-нолики
    
    * Объединение, пересечение, разность множеств + наименьшее положительное число, не предствимое суммой элементов

---
+++ ProgrammingBasics.Domain/Arrays/LectureTasks.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Linq;
+
+namespace ProgrammingBasics.Domain.Arrays
+{
+    public class LectureTasks
+    {
+        /// <summary>
+        /// Метод поиска минимума в массиве.
+        /// </summary>
+        /// <param name="array">Массим.</param>
+        /// <returns>Значение минимального элемента.</returns>
+        public double Min(double[] array)
+        {
+            var min = double.MaxValue;
+            foreach (var item in array)
+                if (item < min) min = item;
+            return min;
+        }
+
+        /// <summary>
+        /// Метод возвращает индекс максимального элемента в массиве.
+        /// </summary>
+        /// <param name="array">Массив.</param>
+        /// <returns>Индекс максимального элемента.</returns>
+        public int GetIndexOfMaxElement(double[] array)
+        {
+            if (!array.Any())
+                return -1;
+
+            return Array.IndexOf(array, array.Max());
+        }
+
+        /// <summary>
+        /// Количество вхождений элемента в массив.
+        /// </summary>
+        /// <param name="items">Массив элементов.</param>
+        /// <param name="itemToCount">Искомый элемент.</param>
+        /// <returns>Количество.</returns>
+        public int GetElementCount(int[] items, int itemToCount)
+        {
+            return items.Count(x => x == itemToCount);
+        }
+
+        /// <summary>
+        /// Поиск начального индекса подмассива.
+        /// </summary>
+        /// <param name="array">Массив.</param>
+        /// <param name="subArray">Искомый подмассив.</param>
+        /// <returns>Начальный индекс в массиве, с которого начинается подмассив.</returns>
+        public int FindSubarrayStartIndex(int[] array, int[] subArray)
+        {
+            for (var i = 0; i < array.Length - subArray.Length + 1; i++)
+                if (ContainsAtIndex(array, subArray, i))
+                    return i;
+            return -1;
+        }
+
+        /// <summary>
+        /// Проверка, сорержится ли подмассив в массиве, начиная с указанного индекса.
+        /// </summary>
+        /// <param name="array">Массив, в котором ведется поиск подмассива.</param>
+        /// <param name="subArray">Подмассив.</param>
+        /// <param name="i">Индекс, начиная с которого, как предполагается, начинается подмассив.</param>
+        /// <returns>Содержится/Не содержится.</returns>
+        private bool ContainsAtIndex(int[] array, int[] subArray, int i)
+        {
+            for (int j = i, k = 0; j < array.Length && k < subArray.Length; j++, k++)
+            {
+                if (array[j] != subArray[k])
+                    return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Руссификация.
+        /// </summary>
+        /// <param name="suit">Масть карты.</param>
+        /// <returns>Русское название масти.</returns>
+        public string GetSuit(Suits suit)
+            => suit switch
+            {
+                Suits.Wands => "жезлов",
+                Suits.Coins => "монет",
+                Suits.Cups => "кубков",
+                Suits.Swords => "мечей",
+                _ => throw new ArgumentException(nameof(suit))
+            };
+
+        /// <summary>
+        /// Крестики-нолики
+        /// </summary>
+        /// <param name="field">Поле игры.</param>
+        /// <returns>Результат игры.</returns>
+        public GameResult GetGameResult(Mark[,] field)
+        {
+            bool circleCheck = CheckMarkToWin(field, Mark.Circle);
+            bool crossCheck = CheckMarkToWin(field, Mark.Cross);
+
+            if (circleCheck && crossCheck)
+                return GameResult.Draw;
+
+            if (circleCheck)
+                return GameResult.CircleWin;
+
+            if (crossCheck)
+                return GameResult.CrossWin;
+
+            return GameResult.Draw;
+        }
+
+        /// <summary>
+        /// Проверка значения (крестик/нолик) на выигрыш.
+        /// </summary>
+        /// <param name="field">Поле игры.</param>
+        /// <param name="mark">Проверяемое на выигрыш значение (крестик/нолик).</param>
+        /// <returns>Выигрыш/нет выигрыша.</returns>
+        private bool CheckMarkToWin(Mark[,] field, Mark mark)
+        {
+            for (int i = 0; i < field.GetLength(0); i++)
+                if (CheckRow(field, mark, i))
+                    return true;
+
+            for (int i = 0; i < field.GetLength(1); i++)
+                if (CheckColumn(field, mark, i))
+                    return true;
+
+            return CheckLeftToRightDiagonal(field, mark) || CheckRightToLeftDiagonal(field, mark);
+        }
+
+        /// <summary>
+        /// Проверка выигрышного результата в строке.
+        /// </summary>
+        /// <param name="field">Поле игры.</param>
+        /// <param name="mark">Проверяемое на выигрыш значение (крестик/нолик).</param>
+        /// <param name="rowIndex">Индекс строки в поле.</param>
+        /// <returns>Выигрыш/нет выигрыша.</returns>
+        private bool CheckRow(Mark[,] field, Mark mark, int rowIndex)
+        {
+            for (int i = 0; i < field.GetLength(1); i++)
+                if (field[rowIndex, i] != mark)
+                    return false;
+
+            return true;
+        }
+
+        /// <summary>
+        /// Проверка выигрышного результата в столбце.
+        /// </summary>
+        /// <param name="field">Поле игры.</param>
+        /// <param name="mark">Проверяемое на выигрыш значение (крестик/нолик).</param>
+        /// <param name="colIndex">Индекс столбца в поле.</param>
+        /// <returns>Выигрыш/нет выигрыша.</returns>
+        private bool CheckColumn(Mark[,] field, Mark mark, int colIndex)
+        {
+            for (int i = 0; i < field.GetLength(0); i++)
+                if (field[i, colIndex] != mark)
+                    return false;
+
+            return true;
+        }
+
+        /// <summary>
+        /// Проверка выигрышного результата в диагонали слева - на право.
+        /// </summary>
+        /// <param name="field">Поле игры.</param>
+        /// <param name="mark">Проверяемое на выигрыш значение (крестик/нолик).</param>
+        /// <returns>Выигрыш/нет выигрыша.</returns>
+        private bool CheckLeftToRightDiagonal(Mark[,] field, Mark mark)
+        {
+            for (int i = 0, j = 0; i < field.GetLength(0) && j < field.GetLength(1); i++, j++)
+                if (field[i, j] != mark)
+                    return false;
+
+            return true;
+        }
+
+        /// <summary>
+        /// Проверка выигрышного результата в диагонали справа - на лево.
+        /// </summary>
+        /// <param name="field">Поле игры.</param>
+        /// <param name="mark">Проверяемое на выигрыш значение (крестик/нолик).</param>
+        /// <returns>Выигрыш/нет выигрыша.</returns>
+        private bool CheckRightToLeftDiagonal(Mark[,] field, Mark mark)
+        {
+            for (int i = 0, j = field.GetLength(1) - 1; i < field.GetLength(0) && j > -1; i++, j--)
+                if (field[i, j] != mark)
+                    return false;
+
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// Масти карт.
+    /// </summary>
+    public enum Suits
+    {
+        Wands,
+        Coins,
+        Cups,
+        Swords
+    }
+
+    /// <summary>
+    /// Значения поля (не занято/крестик/нолик).
+    /// </summary>
+    public enum Mark
+    {
+        Empty,
+        Cross,
+        Circle
+    }
+
+    /// <summary>
+    /// Результат игры в крестики-нолики.
+    /// </summary>
+    public enum GameResult
+    {
+        CrossWin,
+        CircleWin,
+        Draw
+    }
+
+}
---
+++ ProgrammingBasics.Domain/Arrays/TasksForSeminar.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProgrammingBasics.Domain.Arrays
+{
+    public class TasksForSeminar
+    {
+        /// <summary>
+        /// Дан массив чисел. Нужно его сдвинуть циклически на K позиций вправо, не используя других массивов.
+        /// </summary>
+        /// <param name="arr">Массив.</param>
+        /// <param name="shiftSize">Сдвиг.</param>
+        /// <returns>Сдвинутый массив.</returns>
+        public int[] ShiftArray(int[] arr, int shiftSize)
+        {
+            var shiftedArr = new int[arr.Length];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i + shiftSize < arr.Length)
+                    shiftedArr[i + shiftSize] = arr[i];
+                else
+                    shiftedArr[(i + shiftSize) % arr.Length] = arr[i];
+            }
+
+            return shiftedArr;
+        }
+
+        /// <summary>
+        /// Перевести число из системы счисления с основанием A в систему с основанием B. Можно считать, что 2 ≤ A, B ≤ 10, а число дано в виде массива цифр.
+        /// </summary>
+        /// <param name="a">Основание системы счисления из которой переводится число.</param>
+        /// <param name="b">Основание системы счисления в которую переводится число.</param>
+        /// <param name="number">Числов в виде массива цифр.</param>
+        /// <returns>Число, получившееся в результатае перевода.</returns>
+        public int[] FromSystemToSystem(int a, int b, int[] number)
+        {
+            /*
+             *  Формула перевода в 10-ю систему счисления:
+             *  A = a(n)*p^n + a(n-1)*p^n-1 + ... + a(0)*p^0
+             *  
+             *  Для перевода из 10-й системы в любую другую
+             *  необходимо определять результат целочисленного деления
+             *  и остаток от деления до тех пор, пока результат деления
+             *  не будет равен 0. Получившаяся в результате нахождения
+             *  остатка от деления последовательность чисел - это искомое
+             *  число (от младших разрядов к старшим).
+            */
+
+            // Переводим в 10-ю
+            int in10 = 0;
+            for (int i = 0; i < number.Length; i++)
+                in10 += number[i] * (int)Math.Pow(a, number.Length - i - 1);
+
+            if (b == 10)
+            {
+                return in10.ToString()
+                    .Select(ch => int.Parse(ch.ToString()))
+                    .ToArray();
+            }
+
+            // Переводим в систему b
+            var sb = new StringBuilder();
+            int devideResult = in10;
+            while (devideResult > 0)
+            {
+                sb.Append((devideResult % b).ToString());
+                devideResult = devideResult / b;
+            }
+
+            return sb.ToString()
+                .Reverse()
+                .Select(ch => int.Parse(ch.ToString()))
+                .ToArray();
+        }
+
+        /// <summary>
+        /// Даны два неубывающих массива чисел. Сформировать неубывающие массивы,
+        /// являющиеся объединением, пересечением и разностью этих двух массивов
+        /// (разность в смысле мультимножеств).
+        /// </summary>
+        /// <param name="arrA">Массив первого множества.</param>
+        /// <param name="arrB">Массив второго множества.</param>
+        /// <returns>Объединение/Пересечение/Разность</returns>
+        public (int[] Union, int[] Intersection, int[] Difference) GetUnionIntersectionDifference(int[] arrA, int[] arrB)
+        {
+            var union = arrA
+                .Union(arrB)
+                .ToArray();
+            Array.Sort(union);
+
+            var intersection = arrA
+                .Intersect(arrB)
+                .ToArray();
+            Array.Sort(intersection);
+
+            var difference = arrA
+                .Except(arrB)
+                .ToArray();
+            Array.Sort(difference);
+
+            return (union, intersection, difference);
+        }
+
+        /// <summary>
+        /// * Дан неубывающий массив положительных целых чисел. Найти наименьшее положительное
+        /// целое число, не представимое в виде суммы элементов этого массива (каждый элемент
+        /// разрешается использовать в сумме только один раз).
+        /// </summary>
+        /// <param name="numbers">Неубывающий массив положительных целых чисел.</param>
+        /// <returns>Наименьшее положительное целое число, не представимое в виде суммы элементов массива</returns>
+        public int GetSmallestNumber(int[] numbers)
+        {
+            /*
+             * До тех пор, пока элементы массива <= sum + 1, их можно представить суммой
+             * предыдущих элементов.
+             * Сдвигаем границу, пока цепочка не прервется.
+            */
+            int border = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] <= border + 1)
+                    border += numbers[i];
+                else
+                    break;
+            }
+            return border + 1;
+        }
+    }
+}
