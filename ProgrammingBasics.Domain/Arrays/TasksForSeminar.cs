517fca115dc987e093abd64af81a288a604ca594
ad.tsay <ad.tsay@outlook.com>
2020-05-11 17:32:49 +0500

    Объединение, пересечение, разность множеств + наименьшее положительное число, не предствимое суммой элементов

--- ProgrammingBasics.Domain/Arrays/TasksForSeminar.cs
+++ ProgrammingBasics.Domain/Arrays/TasksForSeminar.cs
@@ -37,6 +37,18 @@ namespace ProgrammingBasics.Domain.Arrays
         /// <returns>Число, получившееся в результатае перевода.</returns>
         public int[] FromSystemToSystem(int a, int b, int[] number)
         {
+            /*
+             *  Формула перевода в 10-ю систему счисления:
+             *  A = a(n)*p^n + a(n-1)*p^n-1 + ... + a(0)*p^0
+             *  
+             *  Для перевода из 10-й системы в любую другую
+             *  необходимо определять результат целочисленного деления
+             *  и остаток от деления до тех пор, пока результат деления
+             *  не будет равен 0. Получившаяся в результате нахождения
+             *  остатка от деления последовательность чисел - это искомое
+             *  число (от младших разрядов к старшим).
+            */
+
             // Переводим в 10-ю
             int in10 = 0;
             for (int i = 0; i < number.Length; i++)
@@ -49,7 +61,7 @@ namespace ProgrammingBasics.Domain.Arrays
                     .ToArray();
             }
 
-            // Переводим в b
+            // Переводим в систему b
             var sb = new StringBuilder();
             int devideResult = in10;
             while (devideResult > 0)
@@ -62,6 +74,59 @@ namespace ProgrammingBasics.Domain.Arrays
                 .Reverse()
                 .Select(ch => int.Parse(ch.ToString()))
                 .ToArray();
+        }
+
+        /// <summary>
+        /// Даны два неубывающих массива чисел. Сформировать неубывающие массивы,
+        /// являющиеся объединением, пересечением и разностью этих двух массивов
+        /// (разность в смысле мультимножеств).
+        /// </summary>
+        /// <param name="arrA">Массив первого множества.</param>
+        /// <param name="arrB">Массив второго множества.</param>
+        /// <returns>Объединение/Пересечение/Разность</returns>
+        public (int[] Union, int[] Intersection, int[] Difference) GetUnionIntersectionDifference(int[] arrA, int[] arrB)
+        {
+            var union = arrA
+                .Union(arrB)
+                .ToArray();
+            Array.Sort(union);
+
+            var intersection = arrA
+                .Intersect(arrB)
+                .ToArray();
+            Array.Sort(intersection);
+
+            var difference = arrA
+                .Except(arrB)
+                .ToArray();
+            Array.Sort(difference);
+
+            return (union, intersection, difference);
+        }
+
+        /// <summary>
+        /// * Дан неубывающий массив положительных целых чисел. Найти наименьшее положительное
+        /// целое число, не представимое в виде суммы элементов этого массива (каждый элемент
+        /// разрешается использовать в сумме только один раз).
+        /// </summary>
+        /// <param name="numbers">Неубывающий массив положительных целых чисел.</param>
+        /// <returns>Наименьшее положительное целое число, не представимое в виде суммы элементов массива</returns>
+        public int GetSmallestNumber(int[] numbers)
+        {
+            /*
+             * До тех пор, пока элементы массива <= sum + 1, их можно представить суммой
+             * предыдущих элементов.
+             * Сдвигаем границу, пока цепочка не прервется.
+            */
+            int border = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] <= border + 1)
+                    border += numbers[i];
+                else
+                    break;
+            }
+            return border + 1;
         }
     }
 }
--- ProgrammingBasics.Tests/Arrays/TasksForSeminarTests.cs
+++ ProgrammingBasics.Tests/Arrays/TasksForSeminarTests.cs
@@ -24,5 +24,35 @@ namespace ProgrammingBasics.Tests.Arrays
 
             Assert.Equal<int[]>(new[] { 1, 1 }, result);
         }
+
+        [Fact]
+        public void GetUnionIntersectionDifferenceSimpleTest()
+        {
+            var task = new TasksForSeminar();
+
+            var result = task.GetUnionIntersectionDifference(new[] { 1, 3, 4, 7, 9 }, new[] { 3, 4, 5, 8, 10 });
+
+            Assert.Equal<int[]>(new[] { 1, 3, 4, 5, 7, 8, 9, 10 }, result.Union);
+            Assert.Equal<int[]>(new[] { 3, 4 }, result.Intersection);
+            Assert.Equal<int[]>(new[] { 1, 7, 9 }, result.Difference);
+        }
+
+        [Theory]
+        [InlineData(new[] { 1, 3, 3, 5, 7, 9 }, 2)]
+        [InlineData(new[] { 1, 2, 5 }, 4)]
+        [InlineData(new[] { 1, 3 }, 2)]
+        [InlineData(new[] { 1, 2, 4, 6 }, 14)]
+        [InlineData(new[] { 1, 2, 4, 8 }, 16)]
+        [InlineData(new[] { 1, 2, 4 }, 8)]
+        [InlineData(new[] { 2, 3, 4, 5 }, 1)]
+        [InlineData(new int[] { }, 1)]
+        public void GetSmallestNumberSimpleTest(int[] numbers, int smullestNumber)
+        {
+            var task = new TasksForSeminar();
+
+            int result = task.GetSmallestNumber(numbers);
+
+            Assert.Equal(smullestNumber, result);
+        }
     }
 }
